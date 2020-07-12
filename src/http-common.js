c97f1f63e7062d352cf07c1514bd59ec134aebff
Lucas Teixeira <sobrinho1@gmail.com>
2020-07-12 11:57:28 -0300

    melhorias

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
-export default axios.create({ 
+export default axios.create({    
   baseURL: 'https://grades-api-12-07-2020.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
