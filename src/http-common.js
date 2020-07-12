3f4e79e4d7cc1daf7eb6073d95f093c31bd618cf
Lucas Teixeira <sobrinho1@gmail.com>
2020-07-12 11:36:48 -0300

    mehorias

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({ 
-  baseURL: 'https://grades-api-12-07-2020.herokuapp.com:443/',
+  baseURL: 'https://grades-api-12-07-2020.herokuapp.com:8081/',
   headers: {
     'Content-type': 'application/json',
   },
