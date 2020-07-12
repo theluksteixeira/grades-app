31e829f914048ce949238324d291b2ec7f3ae6d2
Lucas Teixeira <sobrinho1@gmail.com>
2020-07-12 10:54:46 -0300

    melhorias

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
-export default axios.create({
+export default axios.create({ 
   baseURL: 'https://grades-api-12-07-2020:8081/',
   headers: {
     'Content-type': 'application/json',
